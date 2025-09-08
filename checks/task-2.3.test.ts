c575747dc377a31e28f843dd1b728778b2b940cc
tylerbb812 <tylerbb812@yahoo.com>
2025-09-08 14:38:24 -0600

    update spec

--- checks/task-2.3.test.ts
+++ checks/task-2.3.test.ts
@@ -14,15 +14,17 @@ describe("Task 2.3 - fetchUserData", () => {
 
   it("calls simulateApiCall internally", async () => {
     // Dynamically import so we can spy on simulateApiCall
-    const api = await import("../src/api");
-    const spy = jest.spyOn(api, "simulateApiCall");
+    jest.isolateModules(async () => {
+        const api = await import("../src/api");
+        const spy = jest.spyOn(api, "simulateApiCall");
 
-    await api.fetchUserData("999");
+        await api.fetchUserData("999");
 
-    expect(spy).toHaveBeenCalledWith({
-      id: "999",
-      name: "John Doe",
-      email: "john.doe@example.com",
+        expect(spy).toHaveBeenCalledWith({
+        id: "999",
+        name: "John Doe",
+        email: "john.doe@example.com",
+        });
     });
   });
 });
