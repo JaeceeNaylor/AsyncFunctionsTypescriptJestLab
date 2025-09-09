1a6f06059a3dfd368b05243cfed47d2b2399510f
tylerbb812 <tylerbb812@yahoo.com>
2025-09-09 12:52:50 -0600

    make regex less strict

--- checks/task-5.4.test.ts
+++ checks/task-5.4.test.ts
@@ -27,6 +27,8 @@ describe("Task 5.4 - Add test for getUserProfile with valid token", () => {
 
   it("adds a test for getUserProfile with valid token", () => {
     expect(fileContent).toMatch(/test\(["']getUserProfile with valid token returns profile["']/);
-    expect(fileContent).toMatch(/await expect\(getUserProfile\("mock-token"\)\)\.resolves\.toEqual\s*\({\s*username:\s*["']admin["'],\s*email:\s*["']admin@example.com["']\s*}\s*\)/m);
+    expect(fileContent).toMatch(
+        /await expect\(getUserProfile\("mock-token"\)\)\.resolves\.toEqual\s*\(\s*{\s*username:\s*["']admin["']\s*,\s*email:\s*["']admin@example\.com["']\s*}\s*\)/s
+    );
   });
 });
